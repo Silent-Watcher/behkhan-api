d7d859c34652447744bd849ec0a86e7b257c8748
Silent-Watcher <alitabatabaee20@gmail.com>
2026-07-04 16:53:13 +0330

    fix: fix UserByIdPipe bug to recognize NaN input ids and return error

--- src/modules/user/pipes/user-by-id.pipe.ts
+++ src/modules/user/pipes/user-by-id.pipe.ts
@@ -1,5 +1,10 @@
 import type { ArgumentMetadata, PipeTransform } from '@nestjs/common';
-import { BadRequestException, forwardRef, Inject, NotFoundException } from '@nestjs/common';
+import {
+	BadRequestException,
+	forwardRef,
+	Inject,
+	NotFoundException,
+} from '@nestjs/common';
 import type { UserEntity } from '../user.entity.js';
 import { UserService } from '../user.service.js';
 
@@ -7,15 +12,15 @@ export class UserByIdPipe
 	implements PipeTransform<string, Promise<UserEntity>>
 {
 	constructor(
-        @Inject(forwardRef(() => UserService))
-        private userService: UserService
-    ) {}
+		@Inject(forwardRef(() => UserService))
+		private userService: UserService,
+	) {}
 
 	async transform(
 		value: string,
 		_metadata: ArgumentMetadata,
 	): Promise<UserEntity> {
-		if (Number.isNaN(value)) {
+		if (Number.isNaN(Number(value))) {
 			throw new BadRequestException('Invalid user id format');
 		}
 
